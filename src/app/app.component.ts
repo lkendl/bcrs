d0e68ffd58536db9a6ada1d446a34f6a92c77d9d
Laura Kendl <lkendl@live.com>
2022-09-07 22:57:07 -0600

    added code attributions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,15 @@
+/*
+============================================
+; Title: bcrs
+; File Name: app.component.ts
+; Author: Professor Krasso
+; Date: 7 September 2022
+; Modified By: Seth Kerrey, Laura Kendl
+; Description: The Bob's Computer Repair Shop (BCRS) application calculates
+; service repair fees, generates invoices, and tracks purchases by service. 
+===========================================
+*/
+
 import { Component } from '@angular/core';
 
 @Component({
