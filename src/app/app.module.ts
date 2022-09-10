ffdb2379090eeb4c078ba8de73cdb0fa26d9a818
Laura Kendl <lkendl@live.com>
2022-09-10 11:25:23 -0600

    changed code attribution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@
 ; File Name: app.module.ts
 ; Author: Professor Krasso
 ; Date: 7 September 2022
-; Modified By: Seth Kerrey,& Laura Kendl
+; Modified By: Seth Kerrey, Laura Kendl
 ; Description: The Bob's Computer Repair Shop (BCRS) application calculates
 ; service repair fees, generates invoices, and tracks purchases by service. 
 ===========================================
