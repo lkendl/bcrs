db3002a7c14df866c66204aa0341609f1b6cb0aa
skerrey <sethkerrey@gmail.com>
2022-09-14 22:25:20 -0500

    Update error-response.js

---
+++ server/services/error-response.js
@@ -0,0 +1,29 @@
+/*
+============================================
+; Title: bcrs
+; Author: Professor Krasso
+; Date: 14 September 2022
+; Modified By: Seth Kerrey, Laura Kendl
+; Description: The Bob's Computer Repair Shop (BCRS) application calculates
+; service repair fees, generates invoices, and tracks purchases by service.
+===========================================
+*/
+
+class ErrorResponse {
+  constructor(httpCode, message, data) {
+    this.httpCode = httpCode;
+    this.message = message;
+    this.data = data;
+  }
+
+  toObject() {
+    return {
+      'httpCode': this.httpCode,
+      'message': this.message,
+      'data': this.data,
+      'timestamp': new Date().toLocaleDateString()
+    }
+  }
+}
+
+module.exports = ErrorResponse;
