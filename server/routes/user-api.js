34fd571a8d0a4f03fce4c314d3d0f306b20babc3
skerrey <sethkerrey@gmail.com>
2022-09-14 22:25:15 -0500

    Update user-api.js

---
+++ server/routes/user-api.js
@@ -0,0 +1,19 @@
+/*
+============================================
+; Title: bcrs
+; Author: Professor Krasso
+; Date: 14 September 2022
+; Modified By: Seth Kerrey, Laura Kendl
+; Description: The Bob's Computer Repair Shop (BCRS) application calculates
+; service repair fees, generates invoices, and tracks purchases by service.
+===========================================
+*/
+
+// require statements
+const express = require('express');
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const BaseResponse = require('../services/base-response');
+const ErrorResponse = require('../services/error-response');
+const RoleSchema = require('../schemas/user-role');
+
