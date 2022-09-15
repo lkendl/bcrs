8e8284f5d2b42a479e7903e9bdb77afa5a0c977e
skerrey <sethkerrey@gmail.com>
2022-09-15 15:14:15 -0500

    Update auth.guard.ts

---
+++ src/app/shared/auth.guard.ts
@@ -0,0 +1,33 @@
+/*
+============================================
+; Title: bcrs
+; Author: Professor Krasso
+; Date: 14 September 2022
+; Modified By: Seth Kerrey, Laura Kendl
+; Description: The Bob's Computer Repair Shop (BCRS) application calculates
+; service repair fees, generates invoices, and tracks purchases by service.
+===========================================
+*/
+
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+import { CookieService } from "ngx-cookie-service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router, private cookieService: CookieService) { }
+
+  canActivate(router: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const isAuthenticated = this.cookieService.get('sessionuser')
+
+    if (isAuthenticated) {
+      return true;
+    } else {
+      this.router.navigate(['/session/signin']);
+      return false;
+    }
+  }
+}
