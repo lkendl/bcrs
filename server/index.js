b54c3551c609aa3946721fc8b075577983799d2d
skerrey <sethkerrey@gmail.com>
2022-09-14 22:24:59 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,44 @@
+/*
+============================================
+; Title: bcrs
+; Author: Professor Krasso
+; Date: 14 September 2022
+; Modified By: Seth Kerrey, Laura Kendl
+; Description: The Bob's Computer Repair Shop (BCRS) application calculates
+; service repair fees, generates invoices, and tracks purchases by service.
+===========================================
+*/
+
 /**
  * Require statements
  */
 const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
 const path = require('path');
 const mongoose = require('mongoose');
 
-const app = express(); // Express variable.
+/**
+ * Routes
+ */
+const UserApi = require('./routes/user-api');
+const SessionApi = require('./routes/session-api');
+const SecurityQuestionApi = require('./routes/security-question-api');
 
 /**
  * App configurations.
  */
+let app = express();
 app.use(express.json());
 app.use(express.urlencoded({'extended': true}));
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../dist/bcrs')));
 app.use('/', express.static(path.join(__dirname, '../dist/bcrs')));
 
-// default server port value.
-const PORT = 3000 || process.env.PORT;
+/**
+ * Variables
+ */
+const PORT = process.env.PORT || 3000; // server port
 
 // MongoDB connection
 const CONN = 'mongodb+srv://bcrs_user:s3cret@buwebdev-cluster-1.ixkw5.mongodb.net/bcrs?retryWrites=true&w=majority';
@@ -30,7 +52,27 @@ mongoose.connect(CONN).then(() => {
   console.log('MongoDB Error: ' + err.message);
 });
 
-// Wire-up the Express server.
-app.listen(PORT, () => {
-  console.log('Application started and listening on PORT: ' + PORT);
-})
+mongoose.connect(CONN, {
+  promiseLibrary: require('bluebird'),
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true
+}).then(() => {
+  console.debug(`Connection to the database instance was successful`);
+}).catch(err => {
+  console.log(`MongoDB Error: ${err.message}`)
+}); // end mongoose connection
+
+/**
+ * APIs
+ */
+app.use('/api/users', UserApi);
+app.use('/api/session', SessionApi);
+app.use('/api/security-questions', SecurityQuestionApi);
+
+/**
+ * Create and start server
+ */
+http.createServer(app).listen(port, function() {
+  console.log(`Application started and listening on port: ${port}`)
+}); // end http create server function
