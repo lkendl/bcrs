5bf9e22a4a4c03c0639b3a08f538bb466d011e81
Laura Kendl <lkendl@live.com>
2022-09-14 23:26:53 -0600

    added code

---
+++ src/app/pages/security-question-create/security-question-create.component.ts
@@ -0,0 +1,65 @@
+/*
+============================================
+; Title: bcrs
+; Author: Professor Krasso
+; Date: 14 September 2022
+; Modified By: Seth Kerrey, Laura Kendl
+; Description: The Bob's Computer Repair Shop (BCRS) application calculates
+; service repair fees, generates invoices, and tracks purchases by service.
+===========================================
+*/
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SecurityQuestionService } from './../../shared/security-question.service';
+import { SecurityQuestion } from './../../shared/security-question.interface';
+
+@Component({
+  selector: 'app-security-question-create',
+  templateUrl: './security-question-create.component.html',
+  styleUrls: ['./security-question-create.component.css']
+})
+export class SecurityQuestionCreateComponent implements OnInit {
+  form: FormGroup;
+  this.form = this.fb.group({
+    text: [null, Validators.compose([Validators.required])],
+  });
+
+  constructor(private fb: FormBuilder, private router: Router, private securityQuestionService: SecurityQuestionService) {
+  }
+
+  ngOnInit(): void {
+    // OUTDATED CODE - PLACED BEFORE CONSTRUCTOR
+    // this.form = this.fb.group({
+    //   text: [null, Validators.compose([Validators.required])],
+    // });
+  }
+
+  create(): void {
+    const newSecurityQuestion: SecurityQuestion = {
+      text: this.form.controls.text.value
+    }
+
+    // REPLACE OUTDATED SUBSCRIBE CODE
+    // this.securityQuestionService.createSecurityQuestion(newSecurityQuestion).subscribe(res => {
+    //   this.router.navigate(['/security-questions']);
+    // }, err => {
+    //   console.log(err);
+    // });
+
+    // UPDATED SUBSCRIBE CODE
+    this.securityQuestionService.createSecurityQuestion(newSecurityQuestion).subscribe({
+      next: (res) => {
+        this.router.navigate(['./security-questions'])
+      },
+      error: (e) => {
+        console.log(e);
+      }
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/security-questions']);
+  }
+}
---
+++ src/app/pages/signin/signin.component.ts
@@ -0,0 +1,81 @@
+/*
+============================================
+; Title: bcrs
+; Author: Professor Krasso
+; Date: 14 September 2022
+; Modified By: Seth Kerrey, Laura Kendl
+; Description: The Bob's Computer Repair Shop (BCRS) application calculates
+; service repair fees, generates invoices, and tracks purchases by service.
+===========================================
+*/
+
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+import { Message } from 'primeng/api/message';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+  form: FormGroup;
+  errorMessages: Message[];
+
+  constructor(private router: Router, private cookieService: CookieService, private fb: FormBuilder, private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      userName: [null, Validators.compose([Validators.required])],
+      password: [null, Validators.compose([Validators.required, Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$')])],
+    });
+  }
+
+  signin(): void {
+    const userName = this.form.controls.userName.value;
+    const password = this.form.controls.password.value;
+
+    // REPLACE OUTDATED SUBSCRIBE CODE
+    // this.http.post('/api/session/signin', {
+    //   userName,
+    //   password
+    // }).subscribe(res => {
+    //   console.log(res['data']);
+    //   this.cookieService.set('sessionuser', res['data'].userName, 1);
+    //   this.router.navigate(['/']);
+    //   /*
+    //   if (res['data'].userName) {
+    //   }*/
+    //   }, err => {
+    //     this.errorMessages = [
+    //       { severity: 'error', summary: "Error", detail: err.message }
+    //     ]
+    //     console.log(err);
+    // });
+
+     // UPDATED SUBSCRIBE CODE
+     this.http.post('/api/session/signin', {
+      userName,
+      password
+    }).subscribe({
+      next: (res) => {
+        console.log(res['data']);
+        this.cookieService.set('sessionuser', res['data'].userName, 1);
+        this.router.navigate(['/']);
+        /*
+        if (res['data'].userName) {
+        }*/
+      },
+      error: (e) => {
+        this.errorMessages = [
+          { severity: 'error', summary: "Error", detail: e.message }
+        ]
+        console.log(e);
+      }
+    });
+  }
+}
