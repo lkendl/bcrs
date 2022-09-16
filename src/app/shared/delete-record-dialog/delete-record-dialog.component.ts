42823bd19d6bb7df7b851e37fad8699b9fbed16b
skerrey <sethkerrey@gmail.com>
2022-09-15 20:21:44 -0500

    Update delete-record-dialog.component.ts

--- src/app/shared/delete-record-dialog/delete-record-dialog.component.ts
+++ src/app/shared/delete-record-dialog/delete-record-dialog.component.ts
@@ -8,3 +8,33 @@
 ; service repair fees, generates invoices, and tracks purchases by service. 
 ===========================================
 */
+
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef, DialogData } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-delete-record-dialog',
+  templateUrl: './delete-record-dialog.component.html',
+  styleUrls: ['./delete-record-dialog.component.css']
+})
+export class DeleteRecordDialogComponent implements OnInit {
+  recordId: string;
+  dialogHeader: string;
+  dialogBody: string;
+
+  // constructor(private dialogRef: MatDialogRef<DeleteRecordDialogComponent>, @Inject(MAT_DIALOG_DATA) data) {
+  //   this.recordId = data.recordId;
+  //   this.dialogHeader = data.dialogHeader;
+  //   this.dialogBody = data.dialogBody;
+  // }
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    this.recordId = data.recordId;
+    this.dialogHeader = data.dialogHeader;
+    this.dialogBody = data.dialogBody;
+  }
+
+  ngOnInit(): void {
+
+  }
+}
