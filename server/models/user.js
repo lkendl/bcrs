2a7b9c1fa40f6c389d24bfc66234d8272424e1c2
Laura Kendl <lkendl@live.com>
2022-09-15 21:48:36 -0600

    changed userRoleSchema to UserRoleSchema

--- server/models/user.js
+++ server/models/user.js
@@ -11,7 +11,7 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const userRoleSchema = require('../schemas/user-role');
+const UserRoleSchema = require('../schemas/user-role');
 const SelectedSecurityQuestionSchema = require('../schemas/selected-security-question');
 
 let userSchema = new Schema({
