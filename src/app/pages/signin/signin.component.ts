5abce52c2a6c2eb9badc8a50f6e73c351c73c9ce
skerrey <sethkerrey@gmail.com>
2022-09-19 19:56:56 -0500

    Update signin.component.ts

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -15,8 +15,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Message } from 'primeng/api/message';
 import { Router } from '@angular/router';
-import { User } from 'src/app/shared/models/user.interface';
-import { UserService } from 'src/app/shared/services/user.service';
+import { SessionService } from 'src/app/shared/services/session.service';
 
 @Component({
   selector: 'app-signin',
@@ -27,73 +26,40 @@ export class SigninComponent implements OnInit {
 
   errorMessages: Message[] = [];
 
-  // Create user variable to store the login information.
-  user: User;
-
   // Use the FormBuilder to create a new FormGroup with one FormControl named id.
   signinForm: FormGroup = this.fb.group({
     userName: [null, Validators.compose([Validators.required])],
-    password: [null, Validators.compose([Validators.required, Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$')])] // Adds Angular's built-in Validator to the form.
+    password: [null, Validators.compose([Validators.required, Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$')])]
+    // Adds Angular's built-in Validator to the form.
   })
 
   // Add dependency injections for FormBuilder, Router, cookieService and sessionService.
-  constructor(private router: Router, private cookieService: CookieService, private fb: FormBuilder, private http: HttpClient, private sessionService: UserService) {
-
-    // Define User object as an empty User object.
-    this.user = {} as User;
+  constructor(private router: Router,
+    private cookieService: CookieService,
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private sessionService: SessionService) {
   }
 
   ngOnInit(): void {
   }
 
   signin() {
-    // Get userName and password input values from form.
     const userName = this.signinForm.controls['userName'].value;
     const password = this.signinForm.controls['password'].value;
 
-    // REPLACE OUTDATED SUBSCRIBE CODE
-      // this.http.post('/api/session/signin', {
-      //   userName,
-      //   password
-      // }).subscribe(res => {
-      //   console.log(res['data']);
-      //   this.cookieService.set('sessionuser', res['data'].userName, 1);
-      //   this.router.navigate(['/']);
-      //   /*
-      //   if (res['data'].userName) {
-      //   }*/
-      //   }, err => {
-      //     this.errorMessages = [
-      //       { severity: 'error', summary: "Error", detail: err.message }
-      //     ]
-      //     console.log(err);
-      // });
-
-     // UPDATED SUBSCRIBE CODE - UNCOMMENT TO FIX 'DATA' ERROR
-     this.http.post('/api/session/signin', {
-      userName,
-      password
-      // Call API and subscribe to event.
-    }).subscribe({
-      next: (res:any) => {
-        // If there is a value inside user, it will be true here. If true, add to cookie service. Use .data property from the baseResponse object.
-        if (res['data']) {
-           // Store response data inside the empty employee object.
-           this.user = res['data'];
-        }
-        console.log(res['data']);
-        this.cookieService.set('sessionuser', res['data'].userName, 1);
+    this.sessionService.signin(userName, password).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.cookieService.set('sessionuser', res.data.username, 1);
         this.router.navigate(['/']);
-        /*
-        if (res['data'].userName) {
-        }*/
       },
       error: (e) => {
         this.errorMessages = [
-          { severity: 'error', summary: "Error", detail: e.message }
+          { severity: 'error', summary: 'Error', detail: e.message}
         ]
         console.log(e);
       }
-    });
+    })
   }
 }
