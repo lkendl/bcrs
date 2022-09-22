2b6d53d1428837af2ddbffde57f8f358fde46b62
Laura Kendl <lkendl@live.com>
2022-09-22 15:28:41 -0600

    added MatStepper and MatSelect for register comp.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,17 +12,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 /**
  * Components
  */
 // Pages
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { SecurityQuestionCreateComponent } from './pages/security-question-create/security-question-create.component';
 import { SecurityQuestionDetailsComponent } from './pages/security-question-details/security-question-details.component';
 import { SecurityQuestionListComponent } from './pages/security-question-list/security-question-list.component';
@@ -30,9 +33,6 @@ import { SigninComponent } from './pages/signin/signin.component';
 import { UserCreateComponent } from './pages/user-create/user-create.component';
 import { UserDetailsComponent } from './pages/user-details/user-details.component';
 import { UserListComponent } from './pages/user-list/user-list.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
 
 // Shared
 import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
@@ -46,16 +46,19 @@ import { CookieService } from 'ngx-cookie-service';
  * Angular material imports
  */
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatList, MatListModule } from '@angular/material/list';
 import { MatMenu } from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 // Dialog
 import { DialogModule } from '@angular/cdk/dialog';
@@ -65,6 +68,7 @@ import { DialogModule } from '@angular/cdk/dialog';
  */
 import { MessageModule } from 'primeng/message';
 import { MessagesModule } from 'primeng/messages';
+import { RegisterComponent } from './pages/register/register.component';
 import { TableModule } from 'primeng/table';
 
 @NgModule({
@@ -83,7 +87,8 @@ import { TableModule } from 'primeng/table';
     SigninComponent,
     UserCreateComponent,
     UserDetailsComponent,
-    UserListComponent
+    UserListComponent,
+    RegisterComponent
   ],
   imports: [
     AppRoutingModule,
@@ -99,7 +104,10 @@ import { TableModule } from 'primeng/table';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatListModule,
     MatMenuModule,
+    MatSelectModule,
+    MatStepperModule,
     MatTableModule,
     MatToolbarModule,
     MessageModule,
