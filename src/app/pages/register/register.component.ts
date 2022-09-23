41f27ccbffa7590c05d35770ac1967ee4f54d7a6
Laura Kendl <lkendl@live.com>
2022-09-22 22:44:37 -0600

    added code attributions

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,8 +1,8 @@
-/**
+/*
 ============================================
 ; Title: bcrs
 ; Author: Professor Krasso
-; Date: 21 September 2022
+; Date: 22 September 2022
 ; Modified By: Seth Kerrey, Laura Kendl
 ; Description: The Bob's Computer Repair Shop (BCRS) application calculates
 ; service repair fees, generates invoices, and tracks purchases by service.
--- src/app/pages/reset-password/reset-password.component.ts
+++ src/app/pages/reset-password/reset-password.component.ts
@@ -1,3 +1,14 @@
+/*
+============================================
+; Title: bcrs
+; Author: Professor Krasso
+; Date: 22 September 2022
+; Modified By: Seth Kerrey, Laura Kendl
+; Description: The Bob's Computer Repair Shop (BCRS) application calculates
+; service repair fees, generates invoices, and tracks purchases by service.
+===========================================
+*/
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
