e0bcd495c4f5dff60001011e3ed2c3d7bcda809f
skerrey <sethkerrey@gmail.com>
2022-09-25 17:16:36 -0500

    added reset password API

--- server/routes/session-api.js
+++ server/routes/session-api.js
@@ -406,5 +406,53 @@ router.post('/verify/users/:userName/security-questions', async(req, res) => {
 /**
  * ResetPassword
  */
+router.post('/users/:userName/reset-password', async(req, res) => {
+    try
+    {
+        const password = req.body.password;
+
+        User.findOne({'userName': req.params.userName}, function(err, user)
+        {
+            if (err)
+            {
+                console.log(err);
+                const resetPasswordMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
+                res.status(500).send(resetPasswordMongodbErrorResponse.toObject());
+            }
+            else
+            {
+                console.log(user);
+
+                let hashedPassword = bcrypt.hashSync(password, saltRounds); // salt/hash the password
+
+                user.set({
+                    password: hashedPassword
+                });
+
+                user.save(function(err, updatedUser)
+                {
+                    if (err)
+                    {
+                        console.log(err);
+                        const updatedUserMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
+                        res.status(500).send(updatedUserMongodbErrorResponse.toObject());
+                    }
+                    else
+                    {
+                        console.log(updatedUser);
+                        const updatedPasswordResponse = new BaseResponse(200, 'Query successful', updatedUser);
+                        res.json(updatedPasswordResponse.toObject());
+                    }
+                })
+            }
+        })
+    }
+    catch (e)
+    {
+        console.log(e);
+        const resetPasswordCatchError = new ErrorResponse(500, 'Internal server error', e);
+        res.status(500).send(resetPasswordCatchError.toObject());
+    }
+});
 
 module.exports = router;
