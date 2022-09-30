48dc3c0e7e1d9b3f7f474cc9aed3aa1568cc99bd
Laura Kendl <lkendl@live.com>
2022-09-29 21:48:02 -0600

    updated code

--- src/app/pages/user-details/user-details.component.ts
+++ src/app/pages/user-details/user-details.component.ts
@@ -15,6 +15,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from "src/app/shared/services/user.service";
 import { User } from "src/app/shared/models/user.interface";
 import { RoleService } from './../../shared/services/role.service';
+import { Role } from "../../shared/models/role.interface";
+import { Message } from "primeng/api";
 
 @Component({
   selector: 'app-user-details',
@@ -22,18 +24,33 @@ import { RoleService } from './../../shared/services/role.service';
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent implements OnInit {
-  user!: User;
+
+  user: User;
   userId: string | null;
-  form!: FormGroup;
-  roles: any;
+  errorMessages: Message[];
+  roles: Role[];
+
+  form: FormGroup = this.fb.group({
+    firstName: [null, Validators.compose([Validators.required])],
+    lastName: [null, Validators.compose([Validators.required])],
+    phoneNumber: [null, Validators.compose([Validators.required])],
+    address: [null, Validators.compose([Validators.required])],
+    email: [null, Validators.compose([Validators.required, Validators.email])],
+    role: [null, Validators.compose([Validators.required])]
+  });
 
   constructor(private route: ActivatedRoute, private fb: FormBuilder, private router: Router, private userService: UserService, private roleService: RoleService ) {
-    this.userId = this.route.snapshot.paramMap.get('userId');
+    this.userId = this.route.snapshot.paramMap.get('userId') ?? '';
+    this.user = {} as User;
+    this.errorMessages = [];
+    this.roles = [];
 
   // UPDATED SUBSCRIBE CODE
-  this.userService.findUserById(this.userId!).subscribe({
+  this.userService.findUserById(this.userId).subscribe({
     next: (res) => {
-      this.user = res['data'];
+      this.user = res.data;
+      console.log('User object from findUserById call')
+      console.log(this.user)
     },
     error: (e) => {
       console.log(e);
@@ -44,6 +61,7 @@ export class UserDetailsComponent implements OnInit {
       this.form.controls['phoneNumber'].setValue(this.user.phoneNumber);
       this.form.controls['address'].setValue(this.user.address);
       this.form.controls['email'].setValue(this.user.email);
+      this.form.controls['role'].setValue(this.user.role?.text ?? 'standard');
 
       console.log(this.user);
 
@@ -60,13 +78,6 @@ export class UserDetailsComponent implements OnInit {
 }
 
   ngOnInit(): void {
-    this.form = this.fb.group({
-      firstName: [null, Validators.compose([Validators.required])],
-      lastName: [null, Validators.compose([Validators.required])],
-      phoneNumber: [null, Validators.compose([Validators.required])],
-      address: [null, Validators.compose([Validators.required])],
-      email: [null, Validators.compose([Validators.required, Validators.email])],
-    });
   }
 
   saveUser(): void {
@@ -75,7 +86,10 @@ export class UserDetailsComponent implements OnInit {
       lastName: this.form.controls['lastName'].value,
       phoneNumber: this.form.controls['phoneNumber'].value,
       address: this.form.controls['address'].value,
-      email: this.form.controls['email'].value
+      email: this.form.controls['email'].value,
+      role: {
+        text: this.form.controls['role'].value
+      }
     };
 
   // UPDATED SUBSCRIBE CODE
@@ -84,6 +98,10 @@ export class UserDetailsComponent implements OnInit {
       this.router.navigate(['/users']);
     },
     error: (e) => {
+      this.errorMessages = [
+        { severity: 'error', summary: 'Error', detail: e.message }
+      ]
+      console.log('Node.js server error; httpCode:$(e.httpCode);message:$(e.message)')
       console.log(e);
     }
   });
